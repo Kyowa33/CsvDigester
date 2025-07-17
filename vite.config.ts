b57d06fa8c99d8faacbc7b811234edbe0920b3bf
JunOyag <futuredevs365@gmail.com>
2025-07-17 20:26:36 +0200

    Changed to relative root path (build)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     VitePWA({
@@ -21,7 +22,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: './',
+        start_url: '.',
         icons: [
           {
             src: 'logo192.png',
