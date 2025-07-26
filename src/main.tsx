c355170aa371f691f34a6edb2250980258f9d12a
JunOyag <futuredevs365@gmail.com>
2025-07-26 17:28:29 +0200

    Changed from workbox to electron packageable app

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-//import IntegrityChecker from './IntegrityChecker.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
- {/*   <IntegrityChecker/>*/}
     <App />
   </StrictMode>,
 )
 
-// Check if PWA is ready
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.ready.then(() => {
-      console.log('Service Worker is enabled - The app can be used offline.');
-    });
-  });
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: './',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate', // or 'prompt'
-      devOptions: {
-        enabled: true,
-        type: 'module'
-      },
-      workbox: {
-        globPatterns: ['**/*.{html,css,js,jsx,ts,tsx,ico,png,svg,json,webmanifest}'], 
-      },
-      manifest: {
-        name: 'Offline CSV Digester',
-        short_name: 'CsvDigester',
-        description: 'Encrypt / Decrypt CSV files',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '.',
-        icons: [
-          {
-            src: 'logo192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
   ]
 });
